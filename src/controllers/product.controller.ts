51d31b7e5d350b0fd1025c0153f0b9134b07f5c9
QwesHX <onokosy@gmail.com>
2024-11-25 11:38:01 +0700

    Perbaikan

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import prisma from '../prisma/prismaClient';
 import response from '../utils/response';
 import { createProductDTO, updateProductDTO } from "../interfaces/product.interface";
+import { get } from "http";
 
 export const createProduct = async(req: Request, res: Response) => {
     const { name, description, price, stock }: createProductDTO = req.body;
@@ -24,13 +25,31 @@ export const getProductById = async(req: Request, res: Response) => {
     const id = parseInt(req.params.id)
     const userId = req.user?.userId
 
+    if(!id) {
+        return response(res, 'Bad Request', 'Tambahkan path id', 400)
+    }
+
     if(!userId) {
         return response(res, 'Bad Request', 'User_id tidak ditemukan atau kamu belum login', 400)
     }
 
-    const getProduct = await prisma.product.findUnique({
-        where: {
-            id: id
+    try {
+        
+        const getProduct = await prisma.product.findUnique({
+            where: {
+                id: id
+            }
+        })
+        
+        if(!getProduct) {
+            return response(res, 'Not Found', 'Produk tidak ditemukan', 404)
         }
-    })
+        
+        if(userId !== getProduct.user_id) {
+            return response(res, 'Unauthorized', 'Kamu tidak memiliki izin untuk melihat produk ini', 401)
+        }
+    } catch(error) {
+        console.error(error)
+        response(res, 'Internal Server Error', 'Terjadi kesalahan pada server', 500)
+    }
 }
